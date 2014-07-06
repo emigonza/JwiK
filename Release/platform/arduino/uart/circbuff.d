adf97f67ec9d04f7022f9a3de16ab5c05cc20a96
egonzalez <egonzalez@ergio.com.ar>
2014-07-06 01:18:51 -0300

    Cosmetical

--- Release/platform/arduino/uart/circbuff.d
+++
@@ -1,4 +0,0 @@
-platform/arduino/uart/circbuff.d: ../platform/arduino/uart/circbuff.c \
- ../platform/arduino/uart/circbuff.h
-
-../platform/arduino/uart/circbuff.h:
--- Release/platform/arduino/uart/uart.d
+++
@@ -1,9 +0,0 @@
-platform/arduino/uart/uart.d: ../platform/arduino/uart/uart.c \
- ../platform/arduino/uart/uart.h ../platform/arduino/uart/global.h \
- ../platform/arduino/uart/circbuff.h
-
-../platform/arduino/uart/uart.h:
-
-../platform/arduino/uart/global.h:
-
-../platform/arduino/uart/circbuff.h:
